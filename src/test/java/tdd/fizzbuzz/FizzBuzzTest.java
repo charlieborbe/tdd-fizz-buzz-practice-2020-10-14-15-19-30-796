c8e808a0268afe6506317ee6fa34c1ce2c9734d6
BORBECH <charlieborbs18@gmail.com>
2020-10-15 13:59:06 +0800

    [FizzBuzz][Charlie] feature: Implement the case when given number is equal to 1, return orderNumber

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -8,7 +8,7 @@ public class FizzBuzzTest {
     @Test
     void should_return_1_when_count_off_given_order_number_1() {
         //given
-        int orderNumber = 1
+        int orderNumber = 1;
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         //when
